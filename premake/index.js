13ad64e4551df86c503f09406c9e8d62389167cb
BEASTSM96 <beastboioioi.dev@gmail.com>
2021-09-25 16:17:29 +0100

    Fix windows CI

--- premake/index.js
+++ premake/index.js
@@ -4,16 +4,16 @@ const toolcache = require( '@actions/tool-cache' );
 
 async function main() {
 	if ( process.platform === 'win32' ) {
-		const fileZip = await tc.downloadTool( 'https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-windows.zip' );
-		await tc.extractZip( fileZip, 'tpremake' );
+		const fileZip = await toolcache.downloadTool( 'https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-windows.zip' );
+		await toolcache.extractZip( fileZip, 'tpremake' );
 	  }
 	  else if ( process.platform === 'darwin' ) {
-		const fileZip = await tc.downloadTool( 'https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-macosx.tar.gz' );
-		await tc.extractTar( fileZip, 'tpremake' );
+		const fileZip = await toolcache.downloadTool( 'https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-macosx.tar.gz' );
+		await toolcache.extractTar( fileZip, 'tpremake' );
 	  }
 	  else {
-		const fileZip = await tc.downloadTool( 'https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-linux.tar.gz' );
-		await tc.extractTar( fileZip, 'tpremake' );
+		const fileZip = await toolcache.downloadTool( 'https://github.com/premake/premake-core/releases/download/v5.0.0-alpha16/premake-5.0.0-alpha16-linux.tar.gz' );
+		await toolcache.extractTar( fileZip, 'tpremake' );
 	}
 	core.addPath( "tpremake" )
 }
